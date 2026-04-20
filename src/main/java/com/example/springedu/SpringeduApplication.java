b33d8e1060a570b3564ccbe68ec6ba6b86789994
kangminju <yessuk5499@gmail.com>
2026-04-20 09:34:25 +0900

    first commit

---
+++ src/main/java/com/example/springedu/SpringeduApplication.java
@@ -0,0 +1,13 @@
+package com.example.springedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringeduApplication.class, args);
+	}
+
+}
