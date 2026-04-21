cc6a4c538556cb4ef01017a28e50d68ae439d26f
kangminju <yessuk5499@gmail.com>
2026-04-21 19:37:54 +0900

    ComponentScan 설정

--- src/main/java/com/example/springedu/SpringeduApplication.java
+++ src/main/java/com/example/springedu/SpringeduApplication.java
@@ -2,8 +2,10 @@ package com.example.springedu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.springedu", "thymeleaf.exam"})
 public class SpringeduApplication {
 
 	public static void main(String[] args) {
